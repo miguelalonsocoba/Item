1766424a9aee162bb291bd1ba1b1c092a0a13b95
MAALONSO <miguel.coba@gnp.com.mx>
2020-02-22 20:18:34 -0600

    Agregando configuarion para utilizar el servidor de Eureka

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,18 +2,18 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * The clas Principal de la aplicación. La etiquest @EnableFeignClientes sirve
  * para abilitar los clientes Feing que esten habilitados en el proyecto y
  * permite inyectar estos clientes en los controladores u otros componentes de
- * Spring como los Service.
- * RibbonClient sirve para el valanceo de carga, permite habilitar varios puertos para el uso del service.
+ * Spring como los Service. RibbonClient sirve para el valanceo de carga,
+ * permite habilitar varios puertos para el uso del service.
  * 
  */
-@RibbonClient(name = "servicio-productos")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
